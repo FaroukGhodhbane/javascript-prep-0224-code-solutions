78d1427f5c40f535d57b5c889a3acfb79ee01d83
Farouk Ghodhbane <fghodbane99@gmail.com>
2024-03-06 04:15:04 +0000

    Completed operators-and-expressions exercise

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const number1 = 45;
+const number2 = 74;
+const product = number1 * number2;
+console.log('Value of product: ', product);
+console.log('data type of product: ', typeof product);
+
+const payment = 45;
+const charge = 74;
+const amountRemaining = charge - payment;
+console.log('Value of amountRemaining : ', amountRemaining);
+console.log('data type of amountRemaining: ', typeof amountRemaining);
+
+const tests = 46;
+const assignments = 81;
+const final = 78;
+const grade = (tests + assignments + final) / 3;
+console.log('Value of grade : ', grade);
+console.log('data type of grade: ', typeof grade);
+
+const firstName = 'Farouk';
+const lastName = 'Ghodhbane';
+const fullName = firstName + ' ' + lastName;
+console.log('Value of fullName : ', fullName);
+console.log('data type of fullName: ', typeof fullName);
+
+const pH = 8;
+const isAcidic = pH < 7;
+console.log('Value of isAcidic : ', isAcidic);
+console.log('data type of isAcidic: ', typeof isAcidic);
+
+const numberOfSoldiers = 300;
+const isSparta = numberOfSoldiers === 300;
+console.log('Value of isSparta : ', isSparta);
+console.log('data type of isSparta: ', typeof isSparta);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log('Value of nickname : ', nickname);
+console.log('data type of nickname: ', typeof nickname);
